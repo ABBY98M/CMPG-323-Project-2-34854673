3911b7a3763f37d05b71e9d8f1e9eb3c9e7ea3fe
TeeCee07 <Tebzacomfort11@gmail.com>
2024-08-10 00:26:06 +0200

    Made changes

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Project_2_34854673.Models;
+
+public partial class Client
+{
+    public Guid ClientId { get; set; }
+
+    public string? ClientName { get; set; }
+
+    public string? PrimaryContactEmail { get; set; }
+
+    public DateTime? DateOnboarded { get; set; }
+}
