b78a47f4d9b4d9cb07523ad85cf96d2f1d379927
Abegail Majone <www.aahbeydo@gmail.com>
2024-08-12 12:22:11 +0200

    Added client controller

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_Project_2_34854673.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace API_Project_2_34854673.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly NwutechTrendsContext _context;
+
+        public ClientController(NwutechTrendsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(Guid id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Client>> PostClient(Client client)
+        {
+            _context.Clients.Add(client);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetClient), new { id = client.ClientId }, client);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchClient(Guid id, Client client)
+        {
+            if (id != client.ClientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(client).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(Guid id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clients.Remove(client);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClientExists(Guid id)
+        {
+            return _context.Clients.Any(e => e.ClientId == id);
+        }
+    }
+}
