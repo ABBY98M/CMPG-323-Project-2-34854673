3bcf129128676c804feeb423d8a234baca81bef0
Abegail Majone <www.aahbeydo@gmail.com>
2024-08-12 14:30:57 +0200

    updated

---
+++ Authentication/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace JWTAuthentication.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Add any additional properties needed for your application here
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
