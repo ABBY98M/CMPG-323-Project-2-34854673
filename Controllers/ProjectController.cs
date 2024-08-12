9cf6a85530e2df77d46e0df73b9960330f4c6527
Abegail Majone <www.aahbeydo@gmail.com>
2024-08-12 12:27:24 +0200

    added project controller

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API_Project_2_34854673.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace API_Project_2_34854673.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly NwutechTrendsContext _context;
+
+        public ProjectController(NwutechTrendsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(Guid id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return project;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Project>> PostProject(Project project)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProject), new { id = project.ProjectId }, project);
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> PatchProject(Guid id, Project project)
+        {
+            if (id != project.ProjectId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(Guid id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProjectExists(Guid id)
+        {
+            return _context.Projects.Any(e => e.ProjectId == id);
+        }
+    }
+}
