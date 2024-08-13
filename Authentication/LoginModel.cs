6c7479f6c0ead14e4e49b5ceb3a9ae449e8dada5
Abegail Majone <www.aahbeydo@gmail.com>
2024-08-13 11:59:43 +0200

    updated

---
+++ Authentication/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace API_Project_2_34854673.Models
+{
+    public class LoginModel
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
